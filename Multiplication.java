c25f1356547aa8987cb052167f0430bfb331967a
Paramjot Singh <luckycool625@gmail.com>
2022-08-29 20:07:10 +0530

    Multiplication Program

---
+++ Multiplication.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Multiplication {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter Two Numbers");
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int multiply = a*b;
+    System.out.println("Multiplication of" + a + " and " + b + "  is " + multiply);
+    }
+}
