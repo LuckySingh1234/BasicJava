266d912097ebf9039884d3012dff2cd6a6230ca3
Paramjot Singh <luckycool625@gmail.com>
2022-08-30 10:56:48 +0530

    Simple Star Program

---
+++ SimpleStar.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class SimpleStar {
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("*****");
+         System.out.println("****");
+          System.out.println("***");
+           System.out.println("**");
+            System.out.println("*");
+
+    }
+}
