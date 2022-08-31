e0b5f6044873467cdbe25acbfc605331e69d5c89
Paramjot Singh <luckycool625@gmail.com>
2022-08-31 23:25:45 +0530

    While Natural Numbers

---
+++ WhileNaturalNumbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class WhileNaturalNumbers {
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = 1;
+        while(count<=10) {
+            System.out.print(count+" ");
+            count++;
+        }
+        System.out.println();
+    }
+}
