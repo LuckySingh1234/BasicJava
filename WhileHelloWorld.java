7b134525bacf612042e4c9b2bef4fd5f1aa31f10
Paramjot Singh <luckycool625@gmail.com>
2022-08-31 22:55:00 +0530

    While Hello World

---
+++ WhileHelloWorld.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class WhileHelloWorld {
+    public static void main(String [] args ) {
+        Scanner sc = new Scanner(System.in);
+        int count = 0;
+        while(count<20) {
+            System.out.println("Hello World");
+            count++;
+        }
+    }
+}
