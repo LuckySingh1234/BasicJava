80488ca3588531ae22ee06f9f84bb83d5454d882
Paramjot Singh <luckycool625@gmail.com>
2022-09-14 21:34:04 +0530

    Star Pattern Reverse Half Triangle

---
+++ StarPattern4.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class StarPattern4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows");
+        int n = sc.nextInt();
+        int i,j;
+        for(i = 1; i<=n; i++) {
+            for(j=1; j<=n-i+1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
